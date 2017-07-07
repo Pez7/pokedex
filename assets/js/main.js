526ac5bc9a18a0f1718cbe127e6340ced115bc9c
Maria Paz <maria.pazrojasc@gmail.com>
2017-07-07 18:15:13 -0400

    imagenes y nombres

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$.ajax({
+	url: 'http://pokeapi.co/api/v2/pokemon/',
+	type: 'GET',
+	dataType: 'json',
+	data: {'limit':'718'},
+})
+.done(function(event) {
+	console.log("event");
+	event.results.forEach(function(elemento,imagen){
+		var picture = imagen + 1;
+		$(".poke").append("<div class='poke1'><img src='http://pokeapi.co/media/img/" + picture +".png'>" + elemento.name + "</div>");
+	})
+})
+.fail(function() {
+	console.log("error");
+})
+.always(function() {
+	console.log("complete");
+});
+})
